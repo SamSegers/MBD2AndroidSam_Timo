648002f6170416b5c2d05dbad2258f595ee6e5a3
Timo Hoff <timohoff@live.nl>
2016-04-11 23:42:45 +0200

    added splash screen

---
+++ app/src/main/java/nl/samtimo/pubcrawl/SplashActivity.java
@@ -0,0 +1,34 @@
+package nl.samtimo.pubcrawl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import nl.samtimo.pubcrawl.authentication.LoginActivity;
+
+public class SplashActivity extends Activity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
